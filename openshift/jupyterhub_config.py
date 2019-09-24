81065a2d140264bf7999ed7481c89736b7194c96
Sami Laine <sami.v.laine@aalto.fi>
2019-09-24 18:55:37 +0300

    Move custom css under home directory

--- openshift/jupyterhub_config.py
+++ openshift/jupyterhub_config.py
@@ -3,9 +3,6 @@ c.KubeSpawner.mem_guarantee = '512M'
 c.KubeSpawner.mem_limit = '1024M'
 c.JupyterHub.cleanup_servers = False
 
-c.KubeSpawner.environment = {
-    'JUPYTER_CONFIG_DIR': '/opt/app-root/jupyter'
-}
 c.KubeSpawner.extra_pod_config = {
   'volumes': [
     {
@@ -21,7 +18,7 @@ c.KubeSpawner.extra_container_config = {
   'volumeMounts': [
     {
       'name': 'css',
-      'mountPath': '/opt/app-root/jupyter/custom'
+      'mountPath': '/opt/app-root/src/.jupyter/custom'
     },
   ]
 }
--- scripts/create.sh
+++ scripts/create.sh
@@ -3,5 +3,5 @@ oc new-app \
   --template jupyterhub-quickstart \
   --param APPLICATION_NAME=jupyter \
   --param GIT_REPOSITORY_URL=https://github.com/Gehock/era-ai-demo-notebook \
-  --param JUPYTERHUB_CONFIG="`cat ../openshift/jupyter_config.py)`"
+  --param JUPYTERHUB_CONFIG="$(cat ../openshift/jupyterhub_config.py)"
 oc create -f ../openshift/jupyter-css.yaml
