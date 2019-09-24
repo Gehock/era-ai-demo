0cd40257790d5c01338a981be39e6b2622fdd8a1
Sami Laine <sami.v.laine@aalto.fi>
2019-09-24 13:29:27 +0300

    Simplify create script

--- scripts/create.sh
+++ scripts/create.sh
@@ -3,8 +3,5 @@ oc new-app \
   --template jupyterhub-quickstart \
   --param APPLICATION_NAME=jupyter \
   --param GIT_REPOSITORY_URL=https://github.com/Gehock/era-ai-demo-notebook \
-  --param BUILDER_IMAGE=s2i-minimal-notebook:3.6
+  --param JUPYTERHUB_CONFIG="`cat ../openshift/jupyter_config.py)`"
 oc create -f ../openshift/jupyter-css.yaml
-# The builder creates an empty cfg
-oc delete configmap jupyter-cfg
-oc create -f ../openshift/jupyter-cfg.yaml
