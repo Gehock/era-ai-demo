19531fa134621753f8383475657803d17b05659c
Sami Laine <sami.v.laine@aalto.fi>
2019-09-24 19:02:39 +0300

    Use minimal-notebook-freeze as builder

--- scripts/create.sh
+++ scripts/create.sh
@@ -3,5 +3,6 @@ oc new-app \
   --template jupyterhub-quickstart \
   --param APPLICATION_NAME=jupyter \
   --param GIT_REPOSITORY_URL=https://github.com/Gehock/era-ai-demo-notebook \
-  --param JUPYTERHUB_CONFIG="$(cat ../openshift/jupyterhub_config.py)"
+  --param JUPYTERHUB_CONFIG="$(cat ../openshift/jupyterhub_config.py)" \
+  --param BUILDER_IMAGE=s2i-minimal-notebook-freeze:3.6
 oc create -f ../openshift/jupyter-css.yaml
