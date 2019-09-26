f3afb69e165084c5b11235412220a94ee2ccda41
Sami Laine <sami.v.laine@aalto.fi>
2019-09-26 12:30:41 +0300

    Organise scripts

--- scripts/create-jakevdp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# From https://github.com/jupyter-on-openshift/jupyterhub-quickstart#deploying-with-a-custom-notebook-image
-oc new-app --template jupyterhub-quickstart \
-  --param APPLICATION_NAME=jakevdp \
-  --param GIT_REPOSITORY_URL=https://github.com/jakevdp/PythonDataScienceHandbook \
-  --param BUILDER_IMAGE=s2i-minimal-notebook:3.5
