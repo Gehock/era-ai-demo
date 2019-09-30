23c4214f0aa15473f224bda0c7a340b2e2c6ca7f
Sami Laine <sami.v.laine@aalto.fi>
2019-09-30 12:43:43 +0300

    Fix scripts

--- scripts/setup-acme.sh
+++ scripts/setup-acme.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 oc create -fhttps://raw.githubusercontent.com/tnozicka/openshift-acme/master/deploy/letsencrypt-live/single-namespace/{role,serviceaccount,imagestream,deployment}.yaml
 oc policy add-role-to-user openshift-acme --role-namespace="$(oc project --short)" -z openshift-acme
